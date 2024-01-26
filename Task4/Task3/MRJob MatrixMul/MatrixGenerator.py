dc1efa94ebdce5e8b58d4b3df33d52e28a91e8ae
JorgeReyMelian <90771207+JorgeReyMelian@users.noreply.github.com>
2024-01-26 17:09:36 +0000

    Update MatrixGenerator.py

--- Task4/Task3/MRJob MatrixMul/MatrixGenerator.py
+++ Task4/Task3/MRJob MatrixMul/MatrixGenerator.py
@@ -7,14 +7,12 @@ class MatrixGenerator:
         self.matrix = self.generate_matrix()
 
     def generate_matrix(self):
-        # Generate a random matrix of size (self.rows, self.cols)
         return np.random.randint(1, 100, size=(self.rows, self.cols))
 
     def save_to_txt(self, filename, start_letter):
         with open(filename, 'w') as file:
             for i in range(self.rows):
                 for j in range(self.cols):
-                    # Output in the specified format
                     file.write(f'{start_letter},{i},{j},{self.matrix[i, j]}\n')
 
         print(f"Matrix saved to {filename}")
